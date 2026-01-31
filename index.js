e79d1a59adcaf2fc453e82024b048e77a42b9be7
fmarcos8 <francisco-marcos@hotmail.com>
2026-01-31 01:38:22 -0300

    set empty PARTNERS list

--- index.js
+++ index.js
@@ -2,28 +2,7 @@ import TwitchBot from "./bot/TwitchBot.js"
 import EventSubServer from "./eventsub/EventSubServer.js"
 import config from './config/index.js'
 
-const PARTNERS = [
-	'imorih',
-	'DrarkoN_GM',
-	'hieicwb',
-	'zetesys',
-	'paulinhorf92',
-	'antaresnix',
-	'joline_kistner',
-	'socorrodedeus',
-	'aragaos',
-	'luizzakky',
-	'kaori_game',
-	'sadesabrina_',
-	'brdokaus',
-	'wellz_17',
-	'fabio18_oficial',
-	'yamazakii07',
-	'tiiazona',
-	'the_eduardo_silva',
-	'stelzerplay',
-	'ttv_assassinsfps'
-]
+const PARTNERS = [] // lista de streamers parceiros
 
 const bot = new TwitchBot(config, PARTNERS)
 const eventSub = new EventSubServer(bot, config.EVENTSUB_SECRET)
